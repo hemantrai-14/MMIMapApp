a7606763b4bce5692c9cb7abc31f93cd0c26bd3e
hemantrai-14 <hemantrai.14@gmail.com>
2021-08-27 15:36:53 +0530

    j
    jj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MapPageComponent} from '../app/map-page/map-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'map-page',component:MapPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapPageComponent } from './map-page/map-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapPageComponent
   ],
   imports: [
     BrowserModule,
